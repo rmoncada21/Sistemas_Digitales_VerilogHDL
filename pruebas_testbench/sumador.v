a58888f4208201e24301070544baaf027c2ba9ee
roma21fv <106123923+rmoncada21@users.noreply.github.com>
2023-12-11 22:04:39 -0600

    add files

--- pruebas_testbench/sumador.v
+++ pruebas_testbench/sumador.v
@@ -1,5 +1,12 @@
 // Archivo: sumador4bits.v
-module sumador4bits(input [3:0] a, input [3:0] b, output [4:0] sum);
+`timescale 1ns/1ps 
+
+module sumador(
+    input [3:0] a, 
+    input [3:0] b, 
+    output [4:0] sum
+    );
+    
     assign sum = a + b;
 endmodule
 
