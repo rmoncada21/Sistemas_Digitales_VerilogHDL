dc63ba4124fce30f986d055c9dabe4cdfb3cbd59
roma21fv <106123923+rmoncada21@users.noreply.github.com>
2023-12-23 22:21:11 -0600

    checking testbenh_g_and

--- pruebas_testbench/testbench_g_and.v
+++ pruebas_testbench/testbench_g_and.v
@@ -3,22 +3,26 @@ module testbench_g_and;
 
     // Entradas y salidas del testbench
     reg [1:0] in;
-    wire out:
+    wire out;
 
     // Instancia del módulo bajo prueba
-    g_and uut (D[1], D[0], out);
+    g_and uut (.A(in[1]), .B(in[0]), .Y(out));
 
     // Inicialización de las señales
     initial begin
         $display("A\tB\tY");
+        in = 3'b000;
+        repeat(8) begin
+            #1;  // Esperar un ciclo para que se actualicen las salidas
+            $display("%b\t%b\t%b", in[1], in[0], out);
+            in = in + 1;  // Incrementar la entrada para la próxima iteración
+        end
 
-        // Iterar sobre todas las combinaciones posibles de A y B
-        for (A = 0; A <= 1; A = A + 1) begin
-            for (B = 0; B <= 1; B = B + 1) begin
-                // Evaluar el diseño con la combinación actual
-                #1;  // Esperar un ciclo para que se actualicen las salidas
-                $display("%b\t%b\t%b", A, B, Y);
-            end
+        // También puedes usar un bucle for para las combinaciones
+        for (int i = 0; i < 4; i = i + 1) begin
+            #1;
+            $display("%b\t%b\t%b", in[1], in[0], out);
+            in = in + 1;
         end
 
         $finish;  // Finalizar la simulación
